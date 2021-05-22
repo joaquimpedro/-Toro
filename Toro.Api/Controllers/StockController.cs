229212f0d8010d0e621532dc297a4ecea8dbb97a
Joaquim Pedro <joaquim.pedro@hp.com>
2021-05-22 18:34:22 -0300

    criando estrutura inicial

---
+++ Toro.Api/Controllers/StockController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Toro.Api.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        [HttpGet("trends")]
+        public async Task<IActionResult> Trends()
+        {
+            return Ok("Ola mundo");
+        }
+
+        [HttpGet("order")]
+        public async Task<IActionResult> OrderStock()
+        {
+            return Ok("Ola mundo");
+        }
+    }
+}
---
+++ Toro.Domain/Entities/Stock.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Toro.Domain.Entities
+{
+    class Stock
+    {
+    }
+}
