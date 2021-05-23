57270a8077be3bc708bf9213a885591299970348
Joaquim Pedro <joaquim.pedro@hp.com>
2021-05-23 16:18:05 -0300

    removendo camada de infraestrutura e criando a de persistencia

---
+++ Toro.Persistence/Repositories/StockRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toro.Application.Interfaces;
+using Toro.Domain.Entities;
+
+namespace Toro.Persistence.Repositories
+{
+    public class StockRepository : IStockRepository
+    {
+        public List<Stock> GetTrends()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
