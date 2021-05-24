7b0e51fea04a37c1f5e47f8dcf1343ca8702d9a5
Joaquim Pedro <joaquim.pedro@hp.com>
2021-05-24 08:28:00 -0300

    implementando o controller

--- Toro.Api/Controllers/StockController.cs
+++ Toro.Api/Controllers/StockController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Toro.Application.Queries;
+using Toro.Domain.Commands;
 
 namespace Toro.Api.Controllers
 {
@@ -8,15 +11,19 @@ namespace Toro.Api.Controllers
     public class StockController : ControllerBase
     {
         [HttpGet("trends")]
-        public async Task<IActionResult> Trends()
+        public async Task<IActionResult> Trends([FromServices] IMediator mediator)  
         {
-            return Ok("Ola mundo");
+            var result = await mediator.Send(new GetTrendStocksQuery());
+            return Ok(result);
         }
 
         [HttpGet("order")]
-        public async Task<IActionResult> OrderStock()
+        public async Task<IActionResult> OrderStock(
+            [FromBody] OrderStockCommand command,
+            [FromServices] IMediator mediator)
         {
-            return Ok("Ola mundo");
+            var result = await mediator.Send(command);
+            return  Ok(result);
         }
     }
 }
