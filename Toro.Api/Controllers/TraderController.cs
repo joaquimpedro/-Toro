d0a1cdc03792fd9f18dd398fd33d0f66c5ba0eb1
Joaquim Pedro <joaquim.pedro@hp.com>
2021-05-27 10:51:44 -0300

    criando controller e teste para o Trader

---
+++ Toro.Api/Controllers/TraderController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Toro.Application.Features.Traders;
+
+namespace Toro.Api.Controllers
+{
+    [Route("api/trader")]
+    [ApiController]
+    public class TraderController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTrader(int id, [FromServices] IMediator mediator)
+        {
+            var result = await mediator.Send(new GetTraderByIdQuery(id));
+
+            if(result is null)
+            {
+                return NotFound();
+            }
+
+            if (result.Error)
+            {
+                return BadRequest(result.ErrorMessage);
+            }
+
+            return Ok(result.Data);
+        }
+
+    }
+}
---
+++ Toro.Tests/Controllers/TraderControllerTestShould.cs
@@ -0,0 +1,54 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Toro.Api.Controllers;
+using Toro.Application.Features.Stocks;
+using Toro.Application.Features.Traders;
+using Toro.Application.Response.common;
+using Toro.Application.Response.Stock;
+using Toro.Application.Response.Trader;
+using Xunit;
+
+namespace Toro.Tests.Controllers
+{
+    public class TraderControllerTestShould
+    {
+        [Fact]
+        public async Task GetTrader_AndReturnOk()
+        {
+            var mediator = new Mock<IMediator>();
+            var controller = new TraderController();
+            var response = await Task.FromResult(new BaseResponse<TraderResponse>(
+                new TraderResponse { Name = "João", AccountAmmount = 1000, FinancialAssets = new List<FinancialAssetsResponse>() }
+            ));
+
+            mediator.Setup(m => m.Send(It.IsAny<GetTraderByIdQuery>(), It.IsAny<CancellationToken>())).ReturnsAsync(() => response);
+
+            var result = await controller.GetTrader(1, mediator.Object);
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.IsType<TraderResponse>(okResult.Value);
+        }
+
+        [Fact]
+        public async Task OrderStock_WithInvaidId_ReturnNotFound()
+        {
+            var mediator = new Mock<IMediator>();
+            var controller = new TraderController();
+            var response = await Task.FromResult(new BaseResponse<string>("sucesso"));
+
+            mediator.Setup(m => m.Send(It.IsAny<GetTraderByIdQuery>(), It.IsAny<CancellationToken>())).ReturnsAsync(() => null);
+
+            var result = await controller.GetTrader(1, mediator.Object);
+
+            Assert.IsType<NotFoundResult>(result);
+            
+        }
+    }
+}
